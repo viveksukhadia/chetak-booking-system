54dc79fff6d6ac37851c4d3898074a715ee7b787
tgg-viveksukhadiya <vivek.sukhadiya@thegeniusgroup.com.au>
2026-09-12 03:12:11 +0530

    chore: change default web route to return API status json

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -3,5 +3,10 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return response()->json([
+        'name' => 'Chetak Booking System API',
+        'status' => 'online',
+        'version' => '1.0.0',
+        'frontend_url' => 'http://localhost:5173'
+    ]);
 });
